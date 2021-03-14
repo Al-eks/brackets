4ff694c0692905569c3e7729faef225a9cca087e
Alex <vouchankaaleks@gmail.com>
2021-03-14 13:38:58 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if ( str.length % 2 !== 0 ) return false
+  for ( let i = 0; i < bracketsConfig.length; i++ ) {
+    let double = bracketsConfig[i].join('')
+    if ( str.includes(double) ) {
+      str = str.replace(double, '')
+      i = -1
+    }
+  }
+  return str.length === 0
 }
